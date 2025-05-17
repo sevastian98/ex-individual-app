1b99feac503fd25d71a8ebfb699c9165625eded3
Sevastian Vascan <sevastian.vascan7@gmail.com>
2025-05-17 15:51:05 +0300

    Initial commit

---
+++ src/main/java/individual_app/individual_application/IndividualApplication.java
@@ -0,0 +1,13 @@
+package individual_app.individual_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/individual_app/individual_application/IndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package individual_app.individual_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
